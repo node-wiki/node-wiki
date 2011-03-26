cb41619e24bf8c55eb449f3fa43dc0972c04ee1d
Brandon R. Stoner <monokrome@monokro.me>
2011-03-25 21:13:46 -0600

    Added a stack module to avoid some confusion.

---
+++ middleware/stack.js
@@ -0,0 +1,6 @@
+/**
+ * Currently, stack's middleware format is consistent with the connect
+ * implementation - so we can happily share them.
+ */
+module.exports = require('wiki/middleware/connect')
+
