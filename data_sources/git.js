2025ce9672c1ce9645ce5624dd3a65afe7bf29ce
Brandon R. Stoner <monokrome@monokro.me>
2011-03-27 16:23:08 -0600

    Git functionality for find_file is now fully implemented.

--- data_sources/git.js
+++ data_sources/git.js
@@ -45,13 +45,17 @@ function get_initial_template (filename, callback) {
 
 module.exports = {
     find: function find_file(callback, search_path, _settings) {
-        var settings = _settings || {},
+        var path_separator = path.join('a', 'b')[1], // Please add this to node, Ryah ;)
+            settings = _settings || {},
             attempted_create_repository = false,
             attempted_create_branch = false,
+            git_directory_attr = 16384,
             repo, ref_location, ref_list
 
         settings.source = settings.source || get_default_source_settings(settings)
         settings.source.home_template = settings.source.home_template || 'templates/Home.md'
+
+        ref_location = path.join('refs', 'heads', settings.source.branch)
 
         function create_repository (callback) {
             git.initRepository(settings.source.root, true, function (err, _repo) {
@@ -65,7 +69,7 @@ module.exports = {
                             'Created new repository: ' +
                             settings.source.root)
 
-                            // Finally, let's finally read that file.
+                // Finally, let's read that file.
                 callback()
             })
         }
@@ -131,8 +135,6 @@ module.exports = {
          */
         function check_references (callback)
         {
-            ref_location = 'refs/heads/' + settings.source.branch
-
             repo.listReferences(git.GIT_REF_LISTALL, function (err, refs) {
                 if (err) throw err
 
@@ -152,11 +154,115 @@ module.exports = {
         }
 
         /**
+         * Once the tree has been iterated and we have found the proper entry,
+         * this will be used to convert the filename into our final output for
+         * the passing to the callback provided to find_file.
+         */
+        function handle_matching_entry (entry)
+        {
+            if (entry.attributes == git_directory_attr)
+            {
+                throw 'Currently, finding directories is not supported with git.'
+            }
+            else
+            {
+                var extension_index = entry.filename.lastIndexOf('.'),
+                    file_basename = entry.filename.slice(0, extension_index),
+                    file_extension = entry.filename.slice(extension_index+1)
+
+                repo.getRawObject(entry.id, function (err, raw_object) {
+                    if (err) throw err
+
+                    // Finally... Success.
+                    callback(0, [
+                        raw_object.data.toString('utf-8'),
+                        file_extension
+                    ])
+                })
+            }
+        }
+
+        /**
+         * This is the part of the searching system that iterates recursively
+         * through the tree until we've found our search item.
+         */
+        function iterate_tree (tree, search_list) {
+            for (var i=0; i < tree.entryCount; i++)
+            {
+                /**
+                 * We've used a callback factory here in order to prevent any
+                 * collisions in other searches that might be happening in
+                 * serial with this one. The factory provides a closure that
+                 * we can use to separate the global search list from this,
+                 * one and therefore every iteration will only work on it's
+                 * proper search list.
+                 */
+
+                tree.getEntry(i, (function get_entry_factory (search_list) {
+                    return function get_entry (err, entry) {
+                        if (err) throw err
+    
+                        if (search_list.length < 1)
+                            throw ([search_path,
+                                    'not found in ',
+                                    settings.source.root].join(' '))
+    
+                        // First, check if this entry is relevant.
+                        if (entry.filename.indexOf(search_list[0]) === 0)
+                        {
+                            // Now, specific logic for trees or files.
+                            if (entry.attributes == git_directory_attr
+                                && entry.filename == search_list[0])
+                            {
+                                search_list.shift()
+
+                                if (search_list.length === 0)
+                                {
+                                    handle_matching_entry(entry)
+                                }
+                                else
+                                {
+                                    repo.getTree(entry.id, function get_next_tree(err, tree) {
+                                        if (err) throw err
+
+                                        iterate_tree(tree, search_list)
+                                    })
+                                }
+                            }
+                            else if (entry.attributes != git_directory_attr
+                                     && search_list.length === 1)
+                            {
+                                handle_matching_entry(entry)
+                            }
+                        }
+                    }
+
+                })(search_list))
+            }
+        }
+
+        /**
          * Once a repository has been succesfully verified, this goes ahead and
          * finds the requested file within our repository - or throws an error
          * if the file does not exist.
          */
         function search_repository () {
+            // Get our reference to the requested branch
+            repo.getReference(ref_location, function reference_getter (err, ref) {
+                if (err) throw err
+
+                // Next get the target commit for the requested branch
+                repo.getCommit(ref.target, function target_getter (err, target) {
+                  if (err) throw err
+
+                    // Next, get this commit's tree
+                    target.getTree(function tree_getter(err, tree) {
+                        if (err) throw err
+
+                        iterate_tree(tree, search_path.split(path_separator))
+                    })
+                })
+            })
         }
 
         function open_repository () {
@@ -175,6 +281,8 @@ module.exports = {
                         return
                     }
                 }
+
+                repo = _repo
 
                 search_repository()
             })
