f3e8e0577905523c0d0fab93c90357ea2d390a4c
Brandon R. Stoner <monokrome@monokro.me>
2011-03-27 22:40:09 -0600

    Finished abstracting git interactions from the find_file method... I knew that was going to be needed before I even wrote this.

--- data_sources/git.js
+++ data_sources/git.js
@@ -1,3 +1,7 @@
+/***
+ * @TODO: This file is completely a mindfuck. I need to rewrite it when we hit
+ * v0.1.0
+ */
 var git = require('gitteh')
     path = require('path'),
     fs = require('fs'),
@@ -41,6 +45,131 @@ function get_initial_template (filename, callback) {
 
             callback(file_contents)
         })
+    })
+}
+
+function get_ref_location(settings) {
+    return path.join('refs', 'heads', settings.source.branch)
+}
+
+/**
+ * This is a sort of confusing piece. I should probably abstract it out
+ * a bit, but basically we are looking for the `home_template`, getting
+ * it's contents, and then copying them into the git repository for our
+ * first commit.
+ *
+ * @TODO: The "home_template" mechanism is just limiting. We should just
+ * get every file out of a specific directory and copy them over to the
+ * repository one-by-one.
+ */
+function insert_initial_data (callback, repo, settings) {
+    var new_commit = repo.createCommit(),
+        author_info = get_commit_author_data(settings),
+        local_tree = repo.createTree(),
+        blob = repo.createRawObject()
+
+    new_commit.author = new_commit.committer = author_info
+    new_commit.message = 'Initial data.'
+
+    get_initial_template(settings.source.home_template, function (content) {
+        var separator_index = settings.source.home_template.lastIndexOf('/'),
+            blob_filename = settings.source.home_template.slice(separator_index + 1)
+
+        blob.data = new Buffer(content)
+        blob.type = 'blob'
+        blob.save()
+
+        local_tree.addEntry(blob.id, blob_filename, 33188)
+        local_tree.save(function check_tree_saved (err, status) {
+            if (err) throw err
+
+            new_commit.setTree(local_tree)
+            new_commit.save(function check_commit_saved (err, status) {
+                if (err) throw err
+
+                // Since our commit was successful, relate it to our branch.
+                repo.createOidReference(get_ref_location(settings), new_commit.id, function (err, ref) {
+                    if (err) throw err
+
+                    logger.log(logger.levels.INFO,
+                                settings,
+                                'Initial data inserted for ' +
+                                settings.source.root)
+
+                    // Finally, let's finally read that file.
+                    callback(repo, settings)
+                })
+            })
+        })
+    })
+}
+
+function create_repository (callback, settings) {
+    git.initRepository(settings.source.root, true, function (err, repo) {
+        if (err) throw err
+
+        logger.log(logger.levels.INFO,
+                    settings,
+                    'Created new repository: ' +
+                    settings.source.root)
+
+        // Finally, let's read that file.
+        callback(repo)
+    })
+}
+
+/**
+ * This function is called upon succesfully opening a repository in
+ * order for us to ensure that our reference (aka branch) exists within
+ * the repository. If it does not exist, then this will go ahead and
+ * call insert_initial_data to insert any initial data required for this
+ * branch.
+ */
+function check_references (callback, repo, settings)
+{
+    repo.listReferences(git.GIT_REF_LISTALL, function (err, refs) {
+        if (err) throw err
+
+        ref_list = refs.filter(function (ref_name) {
+            if (ref_name === get_ref_location(settings)) return true
+            return false
+        })
+
+        // If our ref_list is empty, then we don't have a ref to use.
+        if (ref_list.length === 0)
+            insert_initial_data(callback, repo, settings)
+
+        // 
+        else
+            callback()
+    })
+}
+
+/**
+ * Open our git repository. If it does not exist and we want git repositories
+ * to be created automatically, then have it created. If it exists, then
+ * begin our search.
+ */
+function open_repository (callback, settings) {
+    git.openRepository(settings.source.root, function repo_opened(err, _repo) {
+        if (err) {
+            if (!settings.source.create_repos || settings.attempted_create_repository)
+            {
+                throw err
+            }
+            else
+            {
+                create_repository(function init_repository (repo) {
+                    check_references(callback, repo, settings)
+                }, settings)
+
+                return
+            }
+        }
+
+        repo = _repo
+
+        callback(repo, settings)
     })
 }
 
@@ -91,7 +220,7 @@ function iterate_tree (callback, repo, tree, conf, search_list) {
                     }
                     else if (entry.attributes != git_directory_attr && search_list.length === 0)
                     {
-                        callback(entry, search_list.length)
+                        callback(repo, entry, search_list.length)
                     }
                 }
             }
@@ -106,115 +235,17 @@ module.exports = {
             settings = _settings || {},
             attempted_create_repository = false,
             attempted_create_branch = false,
-            repo, ref_location, ref_list
+            repo, ref_list
 
         settings.source = settings.source || get_default_source_settings(settings)
         settings.source.home_template = settings.source.home_template || 'templates/Home.md'
-
-        ref_location = path.join('refs', 'heads', settings.source.branch)
-
-        function create_repository (callback) {
-            git.initRepository(settings.source.root, true, function (err, _repo) {
-                if (err) throw err
-
-                // Set repo to our new repository.
-                repo = _repo
-
-                logger.log(logger.levels.INFO,
-                            settings,
-                            'Created new repository: ' +
-                            settings.source.root)
-
-                // Finally, let's read that file.
-                callback()
-            })
-        }
-
-        /**
-         * This is a sort of confusing piece. I should probably abstract it out
-         * a bit, but basically we are looking for the `home_template`, getting
-         * it's contents, and then copying them into the git repository for our
-         * first commit.
-         *
-         * @TODO: The "home_template" mechanism is just limiting. We should just
-         * get every file out of a specific directory and copy them over to the
-         * repository one-by-one.
-         */
-        function insert_initial_data (callback) {
-            var new_commit = repo.createCommit(),
-                author_info = get_commit_author_data(settings),
-                local_tree = repo.createTree(),
-                blob = repo.createRawObject()
-
-            new_commit.author = new_commit.committer = author_info
-            new_commit.message = 'Initial data.'
-
-            get_initial_template(settings.source.home_template, function (content) {
-                var separator_index = settings.source.home_template.lastIndexOf('/'),
-                    blob_filename = settings.source.home_template.slice(separator_index + 1)
-
-                blob.data = new Buffer(content)
-                blob.type = 'blob'
-                blob.save()
-
-                local_tree.addEntry(blob.id, blob_filename, 33188)
-                local_tree.save(function check_tree_saved (err, status) {
-                    if (err) throw err
-
-                    new_commit.setTree(local_tree)
-                    new_commit.save(function check_commit_saved (err, status) {
-                        if (err) throw err
-
-                        // Since our commit was successful, relate it to our branch.
-                        repo.createOidReference(ref_location, new_commit.id, function (err, ref) {
-                            if (err) throw err
-
-                            logger.log(logger.levels.INFO,
-                                        settings,
-                                        'Initial data inserted for ' +
-                                        settings.source.root)
-
-                            // Finally, let's finally read that file.
-                            callback()
-                        })
-                    })
-                })
-            })
-        }
-
-        /**
-         * This function is called upon succesfully opening a repository in
-         * order for us to ensure that our reference (aka branch) exists within
-         * the repository. If it does not exist, then this will go ahead and
-         * call insert_initial_data to insert any initial data required for this
-         * branch.
-         */
-        function check_references (callback)
-        {
-            repo.listReferences(git.GIT_REF_LISTALL, function (err, refs) {
-                if (err) throw err
-
-                ref_list = refs.filter(function (ref_name) {
-                    if (ref_name === ref_location) return true
-                    return false
-                })
-
-                // If our ref_list is empty, then we don't have a ref to use.
-                if (ref_list.length === 0)
-                    insert_initial_data(callback)
-
-                // 
-                else
-                    callback()
-            })
-        }
 
         /**
          * Once the tree has been iterated and we have found the proper entry,
          * this will be used to convert the filename into our final output for
          * the passing to the callback provided to find_file.
          */
-        function handle_matching_entry (entry)
+        function handle_matching_entry (repo, entry)
         {
             if (entry.attributes == git_directory_attr)
             {
@@ -243,9 +274,9 @@ module.exports = {
          * finds the requested file within our repository - or throws an error
          * if the file does not exist.
          */
-        function search_repository () {
+        function search_repository (repo, settings) {
             // Get our reference to the requested branch
-            repo.getReference(ref_location, function reference_getter (err, ref) {
+            repo.getReference(get_ref_location(settings), function reference_getter (err, ref) {
                 if (err) throw err
 
                 // Next get the target commit for the requested branch
@@ -282,35 +313,8 @@ module.exports = {
             })
         }
 
-        /**
-         * Open our git repository. If it does not exist and we want git repositories
-         * to be created automatically, then have it created. If it exists, then
-         * begin our search.
-         */
-        function open_repository () {
-            git.openRepository(settings.source.root, function repo_opened(err, _repo) {
-                if (err) {
-                    if (!settings.source.create_repos || attempted_create_repository)
-                    {
-                        throw err
-                    }
-                    else
-                    {
-                        create_repository(function init_repository () {
-                            check_references(search_repository)
-                        })
 
-                        return
-                    }
-                }
-
-                repo = _repo
-
-                search_repository()
-            })
-        }
-
-        open_repository()
+        open_repository(search_repository, settings)
     },
 
     update: function update_file (callback, filename, settings) {
